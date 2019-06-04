ad1a6998a2165a87ff8972f0a081d24855d99596
zhaoyue <522159200@qq.com>
2019-06-04 19:06:53 +0800

    第一次上传

---
+++ server/js/index.js
@@ -0,0 +1,40 @@
+(function(){
+    var oc=document.getElementsByClassName("city");
+    var od=document.getElementsByClassName("dizhi");
+    console.log(oc)
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    // $("document").ready(function(){
+    //    setInterval(function(){xunhuan()},3000);
+    //    function xunhuan(){
+    //     $(".dong").animate({
+    //         left:-65
+    //     },1000).animate({
+    //         top:-80
+    //     },1000).animate({
+    //         left:-10
+    //     }).animate({
+    //         top:0
+    //     },1000).animate({
+    //         width:90,
+    //         height:90,
+    //         left:-10,
+    //         top:-17
+    //     },1000).animate({
+    //         width:160,
+    //         height:160,
+    //     },1000)
+    //    }
+    // })
+})()
+
+
